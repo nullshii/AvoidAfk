bed1a3fa03116eebe7f3640cefbc95d1e3f1e5c2
nullshii <14684974-nullshii@users.noreply.gitlab.com>
2023-06-24 10:01:16 +0400

    Created project

---
+++ AvoidAfk/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace AvoidAfk.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ AvoidAfk/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AvoidAfk.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
