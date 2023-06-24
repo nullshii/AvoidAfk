47a6eb2301c65fa29807fbb5aaaa58e988d49545
nullshii <14684974-nullshii@users.noreply.gitlab.com>
2023-06-24 12:49:01 +0400

    Added auto mouse movement

--- AvoidAfk/ViewModels/MainWindowViewModel.cs
+++ AvoidAfk/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,53 @@
-﻿namespace AvoidAfk.ViewModels;
+﻿using System.Drawing;
+using System.Reactive;
+using System.Timers;
+using Desktop.Robot;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+
+namespace AvoidAfk.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting => "Welcome to Avalonia!";
+    private readonly Robot _robot;
+    private readonly Timer _timer;
+
+    public MainWindowViewModel()
+    {
+        _robot = new Robot();
+        _timer = new Timer();
+        _timer.Elapsed += MoveMouse;
+
+        Frequency = 5f;
+        IsEnabled = false;
+
+        EnableCommand = ReactiveCommand.Create(Enable, this.WhenAnyValue(x => x.IsEnabled, b => !b));
+        DisableCommand = ReactiveCommand.Create(Disable, this.WhenAnyValue(x => x.IsEnabled));
+    }
+
+    [Reactive] public float Frequency { get; set; }
+    [Reactive] private bool IsEnabled { get; set; }
+
+    public ReactiveCommand<Unit, Unit> EnableCommand { get; set; }
+    public ReactiveCommand<Unit, Unit> DisableCommand { get; set; }
+
+    private void Enable()
+    {
+        IsEnabled = true;
+        _timer.Interval = Frequency * 1000;
+        _timer.Start();
+    }
+
+    private void Disable()
+    {
+        IsEnabled = false;
+        _timer.Stop();
+    }
+
+    private void MoveMouse(object? sender, ElapsedEventArgs elapsedEventArgs)
+    {
+        Point mousePosition = _robot.GetMousePosition();
+        mousePosition.X += 1;
+        _robot.MouseMove(mousePosition);
+    }
 }
